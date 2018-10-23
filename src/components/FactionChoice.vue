87cd6dd1fe480c219cd1d70ad2a4638e4e6c2466
Unknown <riderjensen@gmail.com>
2018-10-23 12:56:48 -0600

    Adding faction choice components

---
+++ src/components/FactionChoice.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <v-flex xs12 sm6>
+      <v-card class="margin">
+        <v-card-title primary-title>
+          <slot></slot>
+        </v-card-title>
+		<v-btn v-on:click="$emit('factionChoice', msg)">Choose</v-btn>
+      </v-card>
+    </v-flex>
+  
+</template>
+
+<script>
+export default {
+  name: "NameCard",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.margin {
+  margin: 20px;
+}
+.factionInt {
+  font-size: 40px;
+}
+</style>
--- src/views/Decide.vue
+++ src/views/Decide.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="about">
     <h1>This is a decider to help you choose a vanilla class</h1>
+	  <v-layout>
+	<FactionChoice msg="alliance" v-on:factionChoice="addFaction">
+		<h1>Alliance</h1>
+		<p>Information about the alliance here</p>
+	</FactionChoice>
+	<FactionChoice msg="horde" v-on:factionChoice="addFaction">
+		<h1>Horde</h1>
+		<p>Information about the horde here</p>
+	</FactionChoice>
+	</v-layout>
   </div>
 </template>
 
 <script>
+import FactionChoice from "@/components/FactionChoice.vue";
+
 export default {
   data: () => {
-    return {};
+    return {
+      userFaction: "",
+      userRace: "",
+      userClass: ""
+    };
+  },
+  components: {
+    FactionChoice
+  },
+  methods: {
+    addFaction(faction) {
+      this.userFaction = faction;
+    }
   }
 };
 </script>
