21cc6cc231b62fc93afc38e14e3514e6feafe255
Rider <riderjensen@gmail.com>
2018-11-28 18:40:31 -0700

    Adding getters, setters, and actions

--- src/store.js
+++ src/store.js
@@ -11,6 +11,55 @@ export default new Vuex.Store({
 		race: '',
 		class: ''
 	},
-	mutations: {},
-	actions: {}
+	getters:{
+		getFirstName: state => {
+			return state.firstName
+		},
+		getLastName: state => {
+			return state.lastName
+		},
+		getFaction: state => {
+			return state.faction
+		},
+		getRace: state => {
+			return state.race
+		},
+		getClass: state => {
+			return state.class
+		}
+	},
+	mutations: {
+		addFirstName(state, x){
+			state.firstName = x
+		},
+		addLastName(state, x){
+			state.lastName = x
+		},
+		addFaction(state, x){
+			state.faction = x
+		},
+		addRace(state, x){
+			state.race = x
+		},
+		addClass(state, x){
+			state.class = x
+		}
+	},
+	actions: {
+		addFirstName({commit}, payload){
+			commit('addFirstName', payload)
+		},
+		addLastName({commit}, payload){
+			commit('addLastName', payload)
+		},
+		addFaction({commit}, payload){
+			commit('addFaction', payload)
+		},
+		addRace({commit}, payload){
+			commit('addRace', payload)
+		},
+		addClass({commit}, payload){
+			commit('addClass', payload)
+		},
+	}
 });
