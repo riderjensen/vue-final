def8747f44f7686157b7ef27a58aeef03c49bbc3
Unknown <riderjensen@gmail.com>
2018-12-10 09:55:57 -0700

    Adding new styles to class choice

--- src/components/ClassChoice.vue
+++ src/components/ClassChoice.vue
@@ -1,12 +1,12 @@
 <template>
-      <v-card class="margin" style="padding-bottom: 20px; height: 93%">
-        <v-card-title primary-title>
-          <slot></slot>
-        </v-card-title>
-		<v-btn v-on:click="$emit('classChoice', msg)">Choose</v-btn>
-      </v-card>
-
-  
+  <v-card class="margin" style="padding-bottom: 20px; height: 93%">
+    <v-card-title primary-title>
+      <slot></slot>
+    </v-card-title>
+    <p style="text-align: center;" class="class-button">
+      <v-btn v-on:click="$emit('classChoice', msg)">Choose</v-btn>
+    </p>
+  </v-card>
 </template>
 
 <script>
@@ -22,8 +22,14 @@ export default {
 <style scoped>
 .margin {
   margin: 20px;
+  padding-bottom: 30px;
 }
 .factionInt {
   font-size: 40px;
+}
+.class-button {
+  position: absolute;
+  bottom: 10px;
+  width: 100%;
 }
 </style>
