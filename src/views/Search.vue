1de913ccec59196b5065aa3ae1b0173a03021a47
Unknown <riderjensen@gmail.com>
2018-12-10 09:55:10 -0700

    Adding extra information

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,16 +1,31 @@
 <template>
-  <div><h1>Generate a character name</h1>
-  <h3 v-if="showName">Your Chosen Name: {{ firstNombre }} {{ lastNomber }}</h3>
-    <UserSearch v-on:userSubmit="searchUser"/>
-	<div v-for="(item, i) in nameArray" :key="i">
-		<transition name="highlight-clock" appear>
-	<NameCard v-bind:gender="item.gender">
-		<h2>{{ item.name }} {{ item.surname }}</h2>
-		<v-spacer></v-spacer>
-		<v-btn color="info" @click="addName(item.name, item.surname)">Select</v-btn>
-	</NameCard>
-		</transition>
-	</div>
+  <div>
+    <h1>Generate a character name</h1>
+    <div>
+      <p>
+        Female names are represented with
+        <span style="color: lightpink">light pink</span> as a background while male names are represented with
+        <span
+          style="color: lightblue"
+        >light blue</span> as a background.
+      </p>
+    </div>
+    <UserSearch v-on:userSubmit="searchUser" style="padding: 20px;"/>
+    <div v-for="(item, i) in nameArray" :key="i">
+      <transition name="highlight-clock" appear>
+        <NameCard v-bind:gender="item.gender">
+          <h2 style="text-align: left;">
+            {{ item.name }} {{ item.surname }}
+            <br>
+            <span style="font-size: 14px;">Origin: {{ item.region }}</span>
+          </h2>
+
+          <v-spacer></v-spacer>
+          <v-btn color="info" @click="addName(item.name, item.surname)">Select</v-btn>
+          <br>
+        </NameCard>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -30,7 +45,6 @@ export default {
       queryString: "?amount=",
       queryAmount: "1",
       nameArray: [],
-      showName: false,
       firstNombre: "",
       lastNomber: ""
     };
@@ -46,6 +60,7 @@ export default {
         )
         .then(response => {
           this.nameArray = response.data;
+          console.log(response.data);
         })
         .catch(function(error) {
           console.log(error);
@@ -56,8 +71,8 @@ export default {
       this.lastNomber = surname;
       this.$store.state.firstName = name;
       this.$store.state.lastName = surname;
-	  this.showName = true;
-	  this.$router.push({ name: "home"})
+      this.showName = true;
+      this.$router.push({ name: "home" });
     }
   }
 };
